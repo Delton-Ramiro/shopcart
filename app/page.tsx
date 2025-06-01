8b304a088913903c27ab25de2084520b36b5c73e
Delton-Ramiro <deltonramiro@gmail.com>
2025-06-01 22:33:03 +0200

    Added banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,10 @@
 import Container from "@/components/Container";
-import { Button } from "@/components/ui/button";
+import { HomeBanner } from "@/components/HomeBanner";
 
 const Home = () => {
   return (
     <Container>
-      <div className="p-10">
-        <h2 className="text-xl font-semibold">Home</h2>
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consequatur
-          debitis laudantium dolor cum eius dicta error delectus voluptates, hic
-          vel! Possimus facere eaque, hic culpa ratione delectus earum
-          doloremque a distinctio molestiae ea, laudantium harum voluptas
-          asperiores repudiandae quos excepturi laboriosam inventore. Adipisci
-          laboriosam consequuntur ex? Expedita velit eveniet laudantium.
-        </p>
-        <Button>Shad CN UI btn</Button>
-      </div>
+      <HomeBanner />
     </Container>
   );
 };
